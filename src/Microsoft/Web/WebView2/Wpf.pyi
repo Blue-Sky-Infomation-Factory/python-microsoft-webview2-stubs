aef70754d53dadf62ec76299e59ffd90ccdb9345
Fina <42113148+BSPR0002@users.noreply.github.com>
2026-03-05 05:13:16 +0800

    Create Wpf.pyi

---
+++ src/Microsoft/Web/WebView2/Wpf.pyi
@@ -0,0 +1,51 @@
+"""
+https://learn.microsoft.com/zh-cn/dotnet/api/microsoft.web.webview2.wpf
+"""
+
+from abc import ABC
+from typing import Final, Optional, Self
+from .Core import CoreWebView2
+from System import IDisposable, Object, EventHandler, Uri
+from System.Windows.Interop import HwndHost
+from Microsoft.Web.WebView2.Core import (
+	CoreWebView2InitializationCompletedEventArgs,
+	CoreWebView2NavigationStartingEventArgs,
+	CoreWebView2NavigationCompletedEventArgs,
+	CoreWebView2WebMessageReceivedEventArgs
+)
+
+class CoreWebView2CreationProperties(Object):
+	def __init__(self):
+		self.AdditionalBrowserArguments: str
+		self.BrowserExecutableFolder: str
+		self.IsInPrivateModeEnabled: bool
+		self.Language: str
+		self.ProfileName: str
+		self.UserDataFolder: str
+
+class IWebView2(IDisposable, ABC):
+	# incomplete
+	@property
+	def CoreWebView2(self) -> Optional[CoreWebView2] :...
+
+class WebView2(HwndHost, IWebView2):
+	# incomplete
+	def __init__(self):
+		self.AllowDrop: Final[bool]
+		self.AllowExternalDrop: bool
+		self.CanGoBack: Final[bool]
+		self.CanGoForward: Final[bool]
+		self.ContextMenu: Final[object]
+		self.ContextMenuStrip: Final[object]
+		self.CoreWebView2: Final[Optional[CoreWebView2]]
+		self.CreationProperties: Optional[CoreWebView2CreationProperties]
+		self.DefaultBackgroundColor: object
+		self.Font: Final[object]
+		self.Source: Uri
+		self.Text: Final[str]
+		self.ZoomFactor: float
+
+	CoreWebView2InitializationCompleted: EventHandler[Self, CoreWebView2InitializationCompletedEventArgs]
+	NavigationCompleted: EventHandler[Self, CoreWebView2NavigationCompletedEventArgs]
+	NavigationStarting: EventHandler[Self, CoreWebView2NavigationStartingEventArgs]
+	WebMessageReceived: EventHandler[Self, CoreWebView2WebMessageReceivedEventArgs]
