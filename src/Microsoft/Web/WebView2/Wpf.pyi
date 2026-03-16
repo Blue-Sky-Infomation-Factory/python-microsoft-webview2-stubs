06241c35928780a37e63765ef3db44ddf2c2b6de
Fina <42113148+BSPR0002@users.noreply.github.com>
2026-03-16 08:58:57 +0800

    Enhance WebView2 pyi with properties and controls
    Update Wpf.pyi to reflect richer WebView2 API: add imports (IKeyboardInputSink, Color, Control, abstractmethod), turn IWebView2 members into typed abstract properties (CoreWebView2, DefaultBackgroundColor, CreationProperties, Source) with getters/setters and event attributes, and move event declarations into the interface. Adjust WebView2 to expose typed property signatures instead of simple attributes. Add WebView2CompositionControl class implementing IWebView2 and IKeyboardInputSink. These changes improve typing and surface new composition control and background color/creation properties in the stub.

--- src/Microsoft/Web/WebView2/Wpf.pyi
+++ src/Microsoft/Web/WebView2/Wpf.pyi
@@ -2,10 +2,12 @@
 https://learn.microsoft.com/zh-cn/dotnet/api/microsoft.web.webview2.wpf
 """
 
-from abc import ABC
+from abc import ABC, abstractmethod
 from typing import Final, Optional, Self
 from .Core import CoreWebView2
-from System import IDisposable, Object, EventHandler, Uri
+from System import IDisposable, IKeyboardInputSink, Object, EventHandler, Uri
+from System.Drawing import Color
+from System.Windows.Controls import Control
 from System.Windows.Interop import HwndHost
 from Microsoft.Web.WebView2.Core import (
 	CoreWebView2InitializationCompletedEventArgs,
@@ -26,7 +28,31 @@ class CoreWebView2CreationProperties(Object):
 class IWebView2(IDisposable, ABC):
 	# incomplete
 	@property
+	@abstractmethod
 	def CoreWebView2(self) -> Optional[CoreWebView2] :...
+	@property
+	@abstractmethod
+	def DefaultBackgroundColor(self) -> Color :...
+	@DefaultBackgroundColor.setter
+	@abstractmethod
+	def DefaultBackgroundColor(self, value: Color) -> None :...
+	@property
+	@abstractmethod
+	def CreationProperties(self) -> Optional[CoreWebView2CreationProperties] :...
+	@CreationProperties.setter
+	@abstractmethod
+	def CreationProperties(self, value: Optional[CoreWebView2CreationProperties]) -> None :...
+	@property
+	@abstractmethod
+	def Source(self) -> Uri :...
+	@Source.setter
+	@abstractmethod
+	def Source(self, value: Uri) -> None :...
+
+	CoreWebView2InitializationCompleted: EventHandler[Self, CoreWebView2InitializationCompletedEventArgs]
+	NavigationCompleted: EventHandler[Self, CoreWebView2NavigationCompletedEventArgs]
+	NavigationStarting: EventHandler[Self, CoreWebView2NavigationStartingEventArgs]
+	WebMessageReceived: EventHandler[Self, CoreWebView2WebMessageReceivedEventArgs]
 
 class WebView2(HwndHost, IWebView2):
 	# incomplete
@@ -35,17 +61,38 @@ class WebView2(HwndHost, IWebView2):
 		self.AllowExternalDrop: bool
 		self.CanGoBack: Final[bool]
 		self.CanGoForward: Final[bool]
-		self.ContextMenu: Final[object]
 		self.ContextMenuStrip: Final[object]
-		self.CoreWebView2: Final[Optional[CoreWebView2]]
-		self.CreationProperties: Optional[CoreWebView2CreationProperties]
-		self.DefaultBackgroundColor: object
 		self.Font: Final[object]
-		self.Source: Uri
 		self.Text: Final[str]
 		self.ZoomFactor: float
 
-	CoreWebView2InitializationCompleted: EventHandler[Self, CoreWebView2InitializationCompletedEventArgs]
-	NavigationCompleted: EventHandler[Self, CoreWebView2NavigationCompletedEventArgs]
-	NavigationStarting: EventHandler[Self, CoreWebView2NavigationStartingEventArgs]
-	WebMessageReceived: EventHandler[Self, CoreWebView2WebMessageReceivedEventArgs]
+	@property
+	def CoreWebView2(self) -> Optional[CoreWebView2] :...
+	@property
+	def DefaultBackgroundColor(self) -> Color :...
+	@DefaultBackgroundColor.setter
+	def DefaultBackgroundColor(self, value: Color) -> None :...
+	@property
+	def CreationProperties(self) -> Optional[CoreWebView2CreationProperties] :...
+	@CreationProperties.setter
+	def CreationProperties(self, value: Optional[CoreWebView2CreationProperties]) -> None :...
+	@property
+	def Source(self) -> Uri :...
+	@Source.setter
+	def Source(self, value: Uri) -> None :...
+
+class WebView2CompositionControl(Control, IKeyboardInputSink, IWebView2):
+	@property
+	def CoreWebView2(self) -> Optional[CoreWebView2] :...
+	@property
+	def DefaultBackgroundColor(self) -> Color :...
+	@DefaultBackgroundColor.setter
+	def DefaultBackgroundColor(self, value: Color) -> None :...
+	@property
+	def CreationProperties(self) -> Optional[CoreWebView2CreationProperties] :...
+	@CreationProperties.setter
+	def CreationProperties(self, value: Optional[CoreWebView2CreationProperties]) -> None :...
+	@property
+	def Source(self) -> Uri :...
+	@Source.setter
+	def Source(self, value: Uri) -> None :...
